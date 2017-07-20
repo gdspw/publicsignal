9527504a5f0bc89de0a5269312954a54331f960f
gongdaoshun <gongdaoshun@192.168.31.178>
2017-07-21 00:08:49 +0800

    初始化项目

---
+++ src/main/java/com/gds/demo/SpringController.java
@@ -0,0 +1,21 @@
+package com.gds.demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class SpringController {
+
+  @RequestMapping("/")
+  @ResponseBody
+  String home(){
+    return "Hello World,Gradle!";
+  }
+  
+  public static void main(String[] args) {
+    SpringApplication.run(SpringController.class, args);
+  }
+}
